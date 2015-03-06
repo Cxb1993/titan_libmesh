a6ade61093ec0224839380c67e91b5766f5deb2c
aghakhani <aghakhaniy63@gmail.com>
2015-03-06 18:39:15 -0500

    Note that there is two way to solve a nonlinear system in libmesh. One is to use a nonlinear solver inside a linear solver, and the other is use a nonlinear solver class

--- titan.C
+++ titan.C
@@ -5,8 +5,17 @@
  *      Author: haghakha
  */
 
+// Unsteady Nonlinear
+// This example shows how a simple, unsteady, nonlinear system of equations
+// can be solved in parallel.  This example introduces the concept of the
+// inner nonlinear loop for each timestep, and requires a good deal of
+// linear algebra number-crunching at each step.
+
 // C++ include files that we need
 #include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <sstream>
 // Functions to initialize the library.
 #include "libmesh/libmesh.h"
 // Basic include files needed for the mesh functionality.
@@ -18,6 +27,7 @@
 // Include files that define a simple steady system
 //#include "libmesh/fem_system.h"
 #include "libmesh/linear_implicit_system.h"
+#include "libmesh/transient_system.h"
 // for reading input parameters from file
 #include "libmesh/getpot.h"
 
@@ -51,6 +61,8 @@
 // you an idea where bottlenecks lie.
 #include "libmesh/perf_log.h"
 
+#include "libmesh/utility.h"
+
 // Bring in everything from the libMesh namespace
 using namespace libMesh;
 
@@ -78,6 +90,9 @@ int main(int argc, char** argv) {
 //	mesh.read (argv[1]);
 	MeshTools::Generation::build_square(mesh, 15, 15, -1., 1., -1., 1., QUAD9);
 
+//	Making mesh 2nd order, this is if we want to go further later
+//	mesh.all_second_order();
+
 //	this is for writing mesh
 //	mesh.write (argv[2]);
 
@@ -94,7 +109,8 @@ int main(int argc, char** argv) {
 
 //	the only system of equation we have
 //	es.add_system<FEMSystem>("SW");
-	LinearImplicitSystem& system = es.add_system<LinearImplicitSystem>("SW");
+	TransientLinearImplicitSystem& system = es.add_system<
+			TransientLinearImplicitSystem>("SW");
 
 //	first we just create height
 //	unsigned int h_var = es.get_system("SW").add_variable("h", SECOND);
@@ -109,17 +125,91 @@ int main(int argc, char** argv) {
 // initialize es
 	es.init();
 
-	es.parameters.set<unsigned int>("linear solver maximum iterations") = 250;
-	es.parameters.set<Real>("linear solver tolerance") = TOLERANCE;
-
 	mesh.print_info();
 
 	es.print_info();
 
-//	es.get_system("SW").solve();
-	system.solve();
+//	Here added when we make it transient
 
-	ExodusII_IO(mesh).write_equation_systems("out.e", es);
+	PerfLog perf_log("Transient Logger");
+
+	es.parameters.set<unsigned int>("linear solver maximum iterations") = 250;
+	es.parameters.set<Real>("linear solver tolerance") = TOLERANCE;
+
+	const Real dt = 0.01;
+	system.time = 0.0;
+	const unsigned int n_timesteps = 15;
+
+	const unsigned int n_nonlinear_steps = 15;
+	const Real nonlinear_tolerance = 1.e-3;
+
+	es.parameters.set<Real>("dt") = dt;
+
+	AutoPtr<NumericVector<Number> > last_nonlinear_soln(
+			system.solution->clone());
+
+	for (unsigned int t_step = 0; t_step < n_timesteps; ++t_step) {
+		system.time += dt;
+
+		std::cout << "\n\n*** Solving time step " << t_step << ", time = "
+				<< system.time << " ***" << std::endl;
+
+		*system.old_local_solution = *system.current_local_solution;
+
+		const Real initial_linear_solver_tol = 1.e-6;
+		es.parameters.set<Real>("linear solver tolerance") =
+				initial_linear_solver_tol;
+
+		for (unsigned int l = 0; l < n_nonlinear_steps; ++l) {
+			last_nonlinear_soln->zero();
+			last_nonlinear_soln->add(*system.solution);
+
+//			perf_log.push("linear solve");
+			es.get_system("SW").solve();
+//			perf_log.pop("linear solve");
+
+			last_nonlinear_soln->add(-1., *system.solution);
+
+			const Real norm_delta = last_nonlinear_soln->l2_norm();
+
+			const unsigned int n_linear_iterations =
+					system.n_linear_iterations();
+
+			const Real final_linear_residual = system.final_linear_residual();
+			std::cout << "Linear solver converged at step: "
+					<< n_linear_iterations << ", final residual: "
+					<< final_linear_residual
+					<< "  Nonlinear convergence: ||u - u_old|| = " << norm_delta
+					<< std::endl;
+
+			if ((norm_delta < nonlinear_tolerance)
+					&& (system.final_linear_residual() < nonlinear_tolerance)) {
+				std::cout << " Nonlinear solver converged at step " << l
+						<< std::endl;
+				break;
+			}
+
+			es.parameters.set<Real>("linear solver tolerance") = std::min(
+					Utility::pow<2>(final_linear_residual),
+					initial_linear_solver_tol);
+
+		} // end nonlinear loop
+
+		const unsigned int write_interval = 1;
+		if ((t_step + 1) % write_interval == 0) {
+			std::ostringstream file_name;
+			file_name << "out_" << std::setw(3) << std::setfill('0')
+					<< std::right << t_step + 1 << ".e";
+
+			ExodusII_IO(mesh).write_equation_systems(file_name.str(), es);
+		}
+
+	} //end timestep loop.
+
+//	steady system solution and write
+//	es.get_system("SW").solve();
+//	system.solve();
+//	ExodusII_IO(mesh).write_equation_systems("out.e", es);
 
 	return 0;
 }
@@ -138,7 +228,8 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 	const unsigned int dim = mesh.mesh_dimension();
 
 	// Get a reference to the Convection-Diffusion system object.
-	LinearImplicitSystem & system = es.get_system<LinearImplicitSystem>("SW");
+	TransientLinearImplicitSystem & system = es.get_system<
+			TransientLinearImplicitSystem>("SW");
 
 	// Numeric ids corresponding to each variable in the system
 	const unsigned int h_var = system.variable_number("h");
@@ -174,6 +265,9 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 	// The element Jacobian * quadrature weight at each integration point.
 	const std::vector<Real>& JxW = fe_h->get_JxW();
 
+	// The element shape functions evaluated at the quadrature points.
+	const std::vector<std::vector<Real> >& phi = fe_h->get_phi();
+
 	// The element shape function gradients for the velocity
 	// variables evaluated at the quadrature points.
 	const std::vector<std::vector<RealGradient> >& dphi = fe_h->get_dphi();
@@ -208,6 +302,19 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 	std::vector<dof_id_type> dof_ind_mx;
 	std::vector<dof_id_type> dof_ind_my;
 
+	// Find out what the timestep size parameter is from the system, and
+	// the value of theta for the theta method.  We use implicit Euler (theta=1)
+	// for this simulation even though it is only first-order accurate in time.
+	// The reason for this decision is that the second-order Crank-Nicolson
+	// method is notoriously oscillatory for problems with discontinuous
+	// initial data such as the lid-driven cavity.  Therefore,
+	// we sacrifice accuracy in time for stability, but since the solution
+	// reaches steady state relatively quickly we can afford to take small
+	// timesteps.  If you monitor the initial nonlinear residual for this
+	// simulation, you should see that it is monotonically decreasing in time.
+	const Real dt = es.parameters.get<Real>("dt");
+	const Real theta = 1.;
+
 	// Now we will loop over all the elements in the mesh that
 	// live on the local processor. We will compute the element
 	// matrix and right-hand-side contribution.  In case users later
@@ -223,7 +330,7 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 		// Store a pointer to the element we are currently
 		// working on.  This allows for nicer syntax later.
 		const Elem* elem = *el;
-		perf_log.push("elem_init");
+//		perf_log.push("elem_init");
 
 		// Get the degree of freedom indices for the
 		// current element.  These define where in the global
@@ -292,43 +399,109 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 		Fmx.reposition(mx_var * n_h_dofs, n_mx_dofs);
 		Fmy.reposition(my_var * n_h_dofs, n_my_dofs);
 
-		perf_log.pop("elem_init");
+//		perf_log.pop("elem_init");
 
 		// Now we will build the element matrix.
 		for (unsigned int qp = 0; qp < qrule.n_points(); qp++) {
-			// Assemble the u-velocity row
-			// uu coupling
-			for (unsigned int i = 0; i < n_h_dofs; i++)
-				for (unsigned int j = 0; j < n_h_dofs; j++)
-					Khh(i, j) += JxW[qp] * (dphi[i][qp] * dphi[j][qp]);
 
-			// up coupling
-			for (unsigned int i = 0; i < n_h_dofs; i++)
-				for (unsigned int j = 0; j < n_my_dofs; j++)
-					Khmy(i, j) += -JxW[qp] * psi[j][qp] * dphi[i][qp](0);
+			// Values to hold the solution & its gradient at the previous timestep.
+			Number h = 0., h_old = 0.;
+			Number mx = 0., mx_old = 0.;
+			Number my_old = 0.;
+			Gradient grad_h, grad_h_old;
+			Gradient grad_mx, grad_mx_old;
 
-			// Assemble the v-velocity row
-			// vv coupling
-			for (unsigned int i = 0; i < n_mx_dofs; i++)
-				for (unsigned int j = 0; j < n_mx_dofs; j++)
-					Kmxmx(i, j) += JxW[qp] * (dphi[i][qp] * dphi[j][qp]);
+			// Compute the velocity & its gradient from the previous timestep
+			// and the old Newton iterate.
+			for (unsigned int l = 0; l < n_h_dofs; l++) {
+				// From the old timestep:
+				h_old += phi[l][qp] * system.old_solution(dof_ind_h[l]);
+				mx_old += phi[l][qp] * system.old_solution(dof_ind_mx[l]);
+				grad_h_old.add_scaled(dphi[l][qp],
+						system.old_solution(dof_ind_h[l]));
+				grad_mx_old.add_scaled(dphi[l][qp],
+						system.old_solution(dof_ind_mx[l]));
 
-			// vp coupling
-			for (unsigned int i = 0; i < n_mx_dofs; i++)
-				for (unsigned int j = 0; j < n_my_dofs; j++)
-					Kmxmy(i, j) += -JxW[qp] * psi[j][qp] * dphi[i][qp](1);
+				// From the previous Newton iterate:
+				h += phi[l][qp] * system.current_solution(dof_ind_h[l]);
+				mx += phi[l][qp] * system.current_solution(dof_ind_mx[l]);
+				grad_h.add_scaled(dphi[l][qp],
+						system.current_solution(dof_ind_h[l]));
+				grad_mx.add_scaled(dphi[l][qp],
+						system.current_solution(dof_ind_mx[l]));
+			}
 
-			// Assemble the pressure row
-			// pu coupling
-			for (unsigned int i = 0; i < n_my_dofs; i++)
-				for (unsigned int j = 0; j < n_h_dofs; j++)
-					Kmyh(i, j) += -JxW[qp] * psi[i][qp] * dphi[j][qp](0);
+			// Compute the old pressure value at this quadrature point.
+			for (unsigned int l = 0; l < n_my_dofs; l++) {
+				my_old += psi[l][qp] * system.old_solution(dof_ind_my[l]);
+			}
 
-			// pv coupling
-			for (unsigned int i = 0; i < n_my_dofs; i++)
-				for (unsigned int j = 0; j < n_mx_dofs; j++)
-					Kmymx(i, j) += -JxW[qp] * psi[i][qp] * dphi[j][qp](1);
+			// Definitions for convenience.  It is sometimes simpler to do a
+			// dot product if you have the full vector at your disposal.
+			const NumberVectorValue H_old(h_old, mx_old);
+			const NumberVectorValue H(h, mx);
+			const Number h_x = grad_h(0);
+			const Number h_y = grad_h(1);
+			const Number mx_x = grad_mx(0);
+			const Number mx_y = grad_mx(1);
 
+			// First, an i-loop over the velocity degrees of freedom.
+			// We know that n_u_dofs == n_v_dofs so we can compute contributions
+			// for both at the same time.
+			for (unsigned int i = 0; i < n_h_dofs; i++) {
+				Fh(i) += JxW[qp] * (h_old * phi[i][qp] -    // mass-matrix term
+						(1. - theta) * dt * (H_old * grad_h_old) * phi[i][qp] + // convection term
+						(1. - theta) * dt * my_old * dphi[i][qp](0) - // pressure term on rhs
+						(1. - theta) * dt * (grad_h_old * dphi[i][qp]) + // diffusion term on rhs
+						theta * dt * (H * grad_h) * phi[i][qp]);  // Newton term
+
+				Fmx(i) += JxW[qp] * (mx_old * phi[i][qp] -  // mass-matrix term
+						(1. - theta) * dt * (H_old * grad_mx_old) * phi[i][qp] + // convection term
+						(1. - theta) * dt * my_old * dphi[i][qp](1) - // pressure term on rhs
+						(1. - theta) * dt * (grad_mx_old * dphi[i][qp]) + // diffusion term on rhs
+						theta * dt * (H * grad_mx) * phi[i][qp]); // Newton term
+
+				// Note that the Fp block is identically zero unless we are using
+				// some kind of artificial compressibility scheme...
+
+				// Matrix contributions for the uu and vv couplings.
+				for (unsigned int j = 0; j < n_h_dofs; j++) {
+					Khh(i, j) += JxW[qp] * (phi[i][qp] * phi[j][qp] + // mass matrix term
+							theta * dt * (dphi[i][qp] * dphi[j][qp]) + // diffusion term
+							theta * dt * (H * dphi[j][qp]) * phi[i][qp] + // convection term
+							theta * dt * h_x * phi[i][qp] * phi[j][qp]); // Newton term
+
+					Khmx(i, j) += JxW[qp] * theta * dt * h_y * phi[i][qp]
+							* phi[j][qp];     // Newton term
+
+					Kmxmx(i, j) += JxW[qp] * (phi[i][qp] * phi[j][qp] + // mass matrix term
+							theta * dt * (dphi[i][qp] * dphi[j][qp]) + // diffusion term
+							theta * dt * (H * dphi[j][qp]) * phi[i][qp] + // convection term
+							theta * dt * mx_y * phi[i][qp] * phi[j][qp]); // Newton term
+
+					Kmxh(i, j) += JxW[qp] * theta * dt * mx_x * phi[i][qp]
+							* phi[j][qp];     // Newton term
+				}
+
+				// Matrix contributions for the up and vp couplings.
+				for (unsigned int j = 0; j < n_my_dofs; j++) {
+					Khmy(i, j) += JxW[qp]
+							* (-theta * dt * psi[j][qp] * dphi[i][qp](0));
+					Kmxmy(i, j) += JxW[qp]
+							* (-theta * dt * psi[j][qp] * dphi[i][qp](1));
+				}
+			}
+
+			// Now an i-loop over the pressure degrees of freedom.  This code computes
+			// the matrix entries due to the continuity equation.  Note: To maintain a
+			// symmetric matrix, we may (or may not) multiply the continuity equation by
+			// negative one.  Here we do not.
+			for (unsigned int i = 0; i < n_my_dofs; i++) {
+				for (unsigned int j = 0; j < n_h_dofs; j++) {
+					Kmyh(i, j) += JxW[qp] * psi[i][qp] * dphi[j][qp](0);
+					Kmymx(i, j) += JxW[qp] * psi[i][qp] * dphi[j][qp](1);
+				}
+			}
 		} // end of the quadrature point qp-loop
 
 		// At this point the interior element integration has
@@ -340,6 +513,9 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 		// the matrix resulting from the L2 projection penalty
 		// approach introduced in example 3.
 		{
+			// The penalty value.  \f$ \frac{1}{\epsilon} \f$
+			const Real penalty = 1.e10;
+
 			// The following loops over the sides of the element.
 			// If the element has no neighbor on a side then that
 			// side MUST live on a boundary of the domain.
@@ -349,22 +525,20 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 
 					// Loop over the nodes on the side.
 					for (unsigned int ns = 0; ns < side->n_nodes(); ns++) {
-						// The location on the boundary of the current
-						// node.
-
-						// const Real xf = side->point(ns)(0);
-						const Real yf = side->point(ns)(1);
-
-						// The penalty value.  \f$ \frac{1}{\epsilon \f$
-						const Real penalty = 1.e10;
-
-						// The boundary values.
+						// Boundary ids are set internally by
+						// build_square().
+						// 0=bottom
+						// 1=right
+						// 2=top
+						// 3=left
 
 						// Set u = 1 on the top boundary, 0 everywhere else
-						const Real u_value = (yf > .99) ? 1. : 0.;
+						const Real h_value =
+								(mesh.get_boundary_info().has_boundary_id(elem,
+										s, 2)) ? 1. : 0.;
 
 						// Set v = 0 everywhere
-						const Real v_value = 0.;
+						const Real mx_value = 0.;
 
 						// Find the node on the element matching this node on
 						// the side.  That defined where in the element matrix
@@ -376,11 +550,25 @@ void assemble_sw(EquationSystems& es, const std::string& system_name) {
 								Kmxmx(n, n) += penalty;
 
 								// Right-hand-side contribution.
-								Fh(n) += penalty * u_value;
-								Fmx(n) += penalty * v_value;
+								Fh(n) += penalty * h_value;
+								Fmx(n) += penalty * mx_value;
 							}
 					} // end face node loop
 				} // end if (elem->neighbor(side) == NULL)
+
+			// Pin the pressure to zero at global node number "pressure_node".
+			// This effectively removes the non-trivial null space of constant
+			// pressure solutions.
+			const bool pin_pressure = true;
+			if (pin_pressure) {
+				const unsigned int pressure_node = 0;
+				const Real my_value = 0.0;
+				for (unsigned int c = 0; c < elem->n_nodes(); c++)
+					if (elem->node(c) == pressure_node) {
+						Kmymy(c, c) += penalty;
+						Fmy(c) += penalty * my_value;
+					}
+			}
 		} // end boundary condition section
 
 		// If this assembly program were to be used on an adaptive mesh,
